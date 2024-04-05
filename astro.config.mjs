ec0062e187949f6472f5da00a35c21d52bc7481d
RicardoCordovilla <ricardocordovillaf@gmail.com>
2024-04-05 03:04:20 -0500

    fix to netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()],
+  prefetch: true,
   output: 'server',
+  adapter: netlify(),
   server: {
+    // port: 3000,
     host: true
-  },
-  integrations: [react()]
+  }
 });
