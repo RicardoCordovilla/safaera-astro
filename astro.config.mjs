4eb6368a20d5415b54f3202a9ae3bac5832fcc38
RicardoCordovilla <ricardocordovillaf@gmail.com>
2024-08-13 19:27:55 -0500

    try fix render

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,17 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 
 // import netlify from "@astrojs/netlify";
-import netlify from '@astrojs/netlify/functions';
+// import netlify from '@astrojs/netlify/functions';
+// import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react({
-    include: ['**/react/*'],
-  }),],
+    include: ['**/react/*']
+  })],
   prefetch: true,
   output: 'server',
-  adapter: netlify({
-    edgeMiddleware: true
-  }),
+  // adapter: vercel(),
   server: {
     // port: 3000,
     host: true
